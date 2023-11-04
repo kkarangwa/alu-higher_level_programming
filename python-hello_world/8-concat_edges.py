412363cb7ba2b4f7bcbab8784ab550c0f8b1fa36
kkarangwa <k.karangwa@alustudent.com>
2023-11-04 14:32:59 -0700

    l

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,3 +3,7 @@ str = "Python is an interpreted, interactive, object-oriented programming\
                 language that combines remarkable power with very clear syntax"
                 str = str[39:67] + str[114:119] + str[:6]
                 print(str)
+
+
+
+
