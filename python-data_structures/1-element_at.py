3365f62128ea244543b9d87b2d06ca257962cbe6
kethia <144065452+kkarangwa@users.noreply.github.com>
2023-11-05 02:23:37 -0800

    Update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,11 @@
-b0VIM 8.1
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])  
