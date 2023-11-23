0e072947c9fcdeb28031352a3919dcd8e52c8793
kethia <144065452+kkarangwa@users.noreply.github.com>
2023-11-23 12:47:54 -0800

    Update 0-rectacngle.py

--- python-more_classes/0-rectacngle.py
+++ python-more_classes/0-rectacngle.py
@@ -1,10 +1,10 @@
-
-dule for class of a rectangle
+#!/usr/bin/python3
+"""Module for class of a rectangle
 
 """
 
 
 class Rectangle():
-        """Simple Rectangle class
+    """Simple Rectangle class
 
-            """
+    """
