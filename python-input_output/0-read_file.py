e8e34365631e78bec729070e9787e37c880c532e
kkarangwa <k.karangwa@alustudent.com>
2023-11-25 02:50:56 -0800

    k

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+mplements a read_file
+Functions:
+        - read_file
+        """
+
+
+        def read_file(file_name=""):
+            """
+                Reads a specified file name and writes its contents
+                    to stdout
+                        Args:
+                                    - file_name(str) - The name of the file to read
+                                        Returns:
+                                                    - None
+                                                        """
+                                                            with open(file_name) as f:
+                                                                    content = f.read()
+                                                                            print(content, end="")
