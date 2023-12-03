b96749620c9d5f7eb3b6630e02fcdfb858125da0
kethia <144065452+kkarangwa@users.noreply.github.com>
2023-12-03 12:30:37 -0800

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
 
 if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status using requests"""
     r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
