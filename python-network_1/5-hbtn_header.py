79d648d3e4266b3b9f6390aed484ca8e3cb3a773
kethia <144065452+kkarangwa@users.noreply.github.com>
2023-12-03 12:31:01 -0800

    Update 5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
+takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the
+response using requests
 """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
+    """takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header
+    of the response using requests"""
+    r = requests.get(argv[1])
     try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
+        r_id = r.headers['X-Request-Id']
+        print(r_id)
     except:
         pass
