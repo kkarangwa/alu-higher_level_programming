59c360a85310918db06832af43950d8a28ab2944
kethia <144065452+kkarangwa@users.noreply.github.com>
2023-12-03 12:31:48 -0800

    Update 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-Python script that sends a request to the URL and
-displays:
-- The body of the response if there are no errors
-- The error code when there is an HTTP error.
+takes in a URL, sends a request to the URL and displays the
+body of the response using requests
 """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
+    """
+    takes in a URL, sends a request to the URL and displays the
+    body of the response using requests
+    """
+    url = argv[1]
+    r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
